f66411eab476172443d6456b4818be583baf20f8
Vandre Leal <vandre.leal.candido@gmail.com>
2020-04-17 08:21:14 -0300

    Updates

---
+++ precache-manifest.e508e00de8e019979a159a406d5d2dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bd5959d2bb765822067fc098bcb546b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71607a1abed34989c956",
+    "url": "/static/js/2.afd5b589.chunk.js"
+  },
+  {
+    "revision": "d2bcf9a9586332c90d24a7fe4e9ddcd2",
+    "url": "/static/js/2.afd5b589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5541d9c0fb873eb607",
+    "url": "/static/js/main.283ca40b.chunk.js"
+  },
+  {
+    "revision": "d0a598c2c4f910599d51",
+    "url": "/static/js/runtime-main.e7310a31.js"
+  },
+  {
+    "revision": "ce5e0dc9a5c5c8dd1a8cbd1a0a0fa4cb",
+    "url": "/static/media/logo.ce5e0dc9.png"
+  }
+]);
